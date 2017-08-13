0253f06250c7cd812adab3e2cc4641144b1c0a67
hesenovzamin <hesenovzamin@gmail.com>
2017-08-13 14:31:03 +0400

    Add files via upload

---
+++ X0/scrpit.js
@@ -0,0 +1,228 @@
+var item1 = document.querySelector("#item1")
+console.log(item1)
+var item2 = document.querySelector("#item2")
+console.log(item2)
+var item3 = document.querySelector("#item3")
+console.log(item3)
+var item4 = document.querySelector("#item4")
+console.log(item4)
+var item5 = document.querySelector("#item5")
+console.log(item5)
+var item6 = document.querySelector("#item6")
+console.log(item6)
+var item7 = document.querySelector("#item7")
+console.log(item7)
+var item8 = document.querySelector("#item8")
+console.log(item8)
+var item9 = document.querySelector("#item9")
+console.log(item9)
+var h1 = document.querySelector("#h1")
+console.log(h1)
+var h2 = document.querySelector("#h2")
+console.log(h2)
+var h3 = document.querySelector("#h3")
+console.log(h3)
+var h4 = document.querySelector("#h4")
+console.log(h4)
+var h5 = document.querySelector("#h5")
+console.log(h5)
+var h6 = document.querySelector("#h6")
+console.log(h6)
+var h7 = document.querySelector("#h7")
+console.log(h7)
+var h8 = document.querySelector("#h8")
+console.log(h8)
+var h9 = document.querySelector("#h9")
+console.log(h9)
+var h10 = document.querySelector("#h10")
+console.log(h10)
+var h11 = document.querySelector("#h11")
+console.log(h11)
+var h12 = document.querySelector("#h12")
+console.log(h12)
+var h13 = document.querySelector("#h13")
+console.log(h13)
+var h14 = document.querySelector("#h14")
+console.log(h14)
+var h15 = document.querySelector("#h15")
+console.log(h15)
+var h16 = document.querySelector("#h16")
+console.log(h16)
+var h17 = document.querySelector("#h17")
+console.log(h17)
+var h18 = document.querySelector("#h18")
+console.log(h18)
+item1.addEventListener("click", myFunc1)
+function myFunc1(){
+h1.style.transition = '2s ease'
+h1.style.fontSize = '9em'
+	if(h1.style.fontSize = '9em')
+	{
+	h10.style.transition = '5s ease'
+	h10.style.fontSize = '9em'
+		item2.addEventListener("click", myFunc2)
+		function myFunc2(){
+		h3.style.transition = '2s ease'
+		h3.style.fontSize = '9em'
+			if(h3.style.fontSize = '9em')
+			{
+			h6.style.transition = '5s ease'
+			h6.style.fontSize = '9em'
+				item7.addEventListener("click", myFunc3)
+				function myFunc3(){
+				h13.style.transition = '2s ease'
+				h13.style.fontSize = '9em'
+					if(h13.style.fontSize = '9em')
+					{
+					h8.style.transition = '5s ease'
+					h8.style.fontSize = '9em'
+						item6.addEventListener("click", myFunc4)
+						function myFunc4(){
+						h11.style.transition = '2s ease'
+						h11.style.fontSize = '9em'
+							if(h13.style.fontSize = '9em')
+							{
+							h16.style.transition = '5s ease'
+							h16.style.fontSize = '9em'
+								item9.addEventListener("click", myFunc5)
+								function myFunc5(){
+								h17.style.transition = '2s ease'
+								h17.style.fontSize = '9em'
+								}			
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+	
+
+
+
+
+
+
+			// item7.addEventListener("click", myFunc3)
+			// function myFunc3(){
+			// h13.style.transition = '2s ease'
+			// h13.style.fontSize = '9em'
+			// if(h13.style.fontSize = '9em')
+			// 	{
+			// 	h8.style.transition = '5s ease'
+			// 	h8.style.fontSize = '9em'
+			// 	}
+	// 				item6.addEventListener("click", myFunc4)
+	// 				function myFunc4(){
+	// 				h11.style.transition = '2s ease'
+	// 				h11.style.fontSize = '9em'
+	// 				if(h13.style.fontSize = '9em')
+	// 					{
+	// 						h16.style.transition = '5s ease'
+	// 						h16.style.fontSize = '9em'
+	// 						item9.addEventListener("click", myFunc5)
+	// 						function myFunc5(){
+	// 						h17.style.transition = '2s ease'
+	// 						h17.style.fontSize = '9em'
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+// item2.addEventListener("click", myFunc2)
+// function myFunc2(){
+//
+
+// 		if(h3.style.fontSize = '9em')
+// 		{
+// 			h6.style.transition = '5s ease'
+// 			h6.style.fontSize = '9em'
+// 		}
+// }
+// item3.addEventListener("click", myFunc3)
+// function myFunc3(){
+// h5.style.transition = '2s ease'
+// h5.style.fontSize = '9em'
+// 		if(h4.style.fontSize = '9em')
+// 		{
+// 			h4.style.transition = '5s ease'
+// 			h4.style.fontSize = '9em'
+// 		}
+// }
+// item4.addEventListener("click", myFunc4)
+// function myFunc4(){
+// h7.style.transition = '2s ease'
+// h7.style.fontSize = '9em'
+// 		if(h14.style.fontSize = '9em')
+// 		{
+// 			h14.style.transition = '5s ease'
+// 			h14.style.fontSize = '9em'
+// 		}
+// }
+// item5.addEventListener("click", myFunc5)
+// function myFunc5(){
+// h9.style.transition = '2s ease'
+// h9.style.fontSize = '9em'
+// 		if(h18.style.fontSize = '9em')
+// 		{
+// 			h18.style.transition = '5s ease'
+// 			h18.style.fontSize = '9em'
+// 		}
+// }
+// item6.addEventListener("click", myFunc6)
+// function myFunc6(){
+// h11.style.transition = '2s ease'
+// h11.style.fontSize = '9em'
+// }
+// item7.addEventListener("click", myFunc7)
+// function myFunc7(){
+// h13.style.transition = '2s ease'
+// h13.style.fontSize = '9em'
+// }
+// item8.addEventListener("click", myFunc8)
+// function myFunc8(){
+// h15.style.transition = '2s ease'
+// h15.style.fontSize = '9em'
+// }
+// item9.addEventListener("click", myFunc9)
+// function myFunc9(){
+// h17.style.transition = '2s ease'
+// h17.style.fontSize = '9em'
+// }
+// item3.addEventListener("click", myFunc3)
+// 	function myFunc3(){
+// 		h5.style.transition = '2s ease'
+// 		h5.style.fontSize = '9em'
+// 		if(h4.style.fontSize = '9em')
+// 		{
+// 		h4.style.transition = '5s ease'
+// 		h4.style.fontSize = '9em'
+// 		}
+// 	}
+// item2.addEventListener("click", myFunc2)
+// 	function myFunc2(){
+// 	h3.style.transition = '2s ease'
+// 	h3.style.fontSize = '9em'
+// 		if(h3.style.fontSize = '9em')
+// 		{
+// 		h6.style.transition = '5s ease'
+// 		h6.style.fontSize = '9em'
+// 		}
+// 	}
+// 	item7.addEventListener("click", myFunc3)
+// 	function myFunc3(){
+// 	h13.style.transition = '2s ease'
+// 	h13.style.fontSize = '9em'
+// 		if(h13.style.fontSize = '9em')
+// 		{
+// 		h8.style.transition = '5s ease'
+// 		h8.style.fontSize = '9em'
+// 		}
+// 	}
